d71c44539417fbd37f1b458fbb1627a0c759a6b0
Zoe <jinsuyi@gmail.com>
2014-02-03 13:30:13 +0100

    DListNode class

---
+++ DListNode.java
@@ -0,0 +1,19 @@
+
+public class DListNode {
+	public String item;
+	public DListNode prev;
+	public DListNode next;
+	
+	DListNode (){
+		item = null;
+		prev = null;
+		next = null;
+	}
+	
+	DListNode (String i){
+		item = i;
+		prev = null;
+		next = null;
+	}
+
+}
