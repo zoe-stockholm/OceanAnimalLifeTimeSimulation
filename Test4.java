92d3ad32a442eb4d57663d3b0823c102251c44ac
Zoe <jinsuyi@gmail.com>
2014-02-03 13:42:36 +0100

    test case

---
+++ Test4.java
@@ -0,0 +1,12 @@
+
+public class Test4 {
+  public static int empty;
+  public static int shark;
+  public static int fish;
+
+  public static void init() {
+    empty = Ocean.EMPTY;
+    shark = Ocean.SHARK;
+    fish = Ocean.FISH;
+  }
+}
